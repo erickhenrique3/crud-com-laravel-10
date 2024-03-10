17b6634976b2f3616ec00deaa2d409b8b8fbfd6b
eric3 <erick.2henriquehtp@gmail.com>
2024-03-10 20:54:14 -0300

    banco de dados conectado com Dbeaver

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -16,20 +17,11 @@ use App\Http\Controllers\UserController;
 
 Route::get(uri: '/', action: [HomeController::class, 'index'])->name('home');
 
-Route::get(uri:'/users', action: [UserController::class, 'index'])->name('users.index');
 
-
-Route::get(uri:'/users/create', action: [UserController::class, 'create'])->name('users.create');
-
-
-Route::post(uri:'/users', action: [UserController::class, 'store'])->name('users.store');
-
-
-Route::post(uri:'/users/{user}', action: [UserController::class, 'show'])->name('users.show');
-
-
-Route::get(uri:'/users/{user}/edit', action: [UserController::class, 'edit'])->name('users.edit');
-
-Route::put(uri:'/users/{user}', action: [UserController::class, 'update'])->name('users.update');
-
-Route::delete(uri:'/users/{user}', action: [UserController::class, 'destroy'])->name('users.destroy');
+Route::get(uri: '/users', action: [UserController::class, 'index'])->name('users.index');
+Route::get(uri: '/users/create', action: [UserController::class, 'create'])->name('users.create');
+Route::post(uri: '/users', action: [UserController::class, 'store'])->name('users.store');
+Route::post(uri: '/users/{user}', action: [UserController::class, 'show'])->name('users.show');
+Route::get(uri: '/users/{user}/edit', action: [UserController::class, 'edit'])->name('users.edit');
+Route::put(uri: '/users/{user}', action: [UserController::class, 'update'])->name('users.update');
+Route::delete(uri: '/users/{user}', action: [UserController::class, 'destroy'])->name('users.destroy');
